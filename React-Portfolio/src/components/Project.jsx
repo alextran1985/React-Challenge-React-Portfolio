5f4c20a61a40b1672e6993c824be169253942fb6
Alex Tran <Lockey5891@aol.com>
2024-09-09 14:04:07 -0700

    added react route

--- React-Portfolio/src/components/Project.jsx
+++ React-Portfolio/src/components/Project.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 
+const projects = [
+  {
+    title: 'One',
+    image: '',
+    deployedLink: '',
+    githubLink: 'http://www.github.com/'
+  },
+  {
+    title: 'Two',
+    image: '',
+    deployedLink: '',
+    githubLink: 'http://www.github.com/ehoverten'
+  },
+  {
+    title: 'Three',
+    image: '',
+    deployedLink: '',
+    githubLink: ''
+  },
+]
+
 const Project = () => {
   return (
-    <div>Project</div>
+    <>
+      <div>Project Component</div>
+      { projects.map(item => (
+        <div className='card'>
+          <h2>{item.title}</h2>
+          <img src={item.image} alt="" />
+          <a href={item.deployedLink}>Project Link</a>
+          <a href={item.deployedLink}>Github Link</a>
+        </div>
+      )) }
+    </>
   )
 }
 
